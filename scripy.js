d09d2230d29c86b48419dd381c007682cec42101
Magoffx2 <107898413+Magoffx2@users.noreply.github.com>
2024-05-28 08:49:34 -0300

    Update scripy.js

--- scripy.js
+++ scripy.js
@@ -1,1 +1,43 @@
+const caixaPrincpal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.queryselector(".caixa-resutado");
+const caixaResultado=document.querySelector(".texto-resultado");
 
+const pergunta[
+}
+enunciado:"como é o nome da filha do neymar? 1",
+  alternativas:[
+"Mavi 1",
+"Bruna 2",
+ ]
+},
+{
+enunciado:"quem é o maior ladrao do Brasil? 2",
+  alternativas:[
+"Bolsonaro",
+"lula",
+ ]
+},
+{
+enunciado:"quem é o dono da empresa tesla? 3",
+  alternativas:[
+"Pablo",
+"Elomusk",
+ ]
+},
+{
+enunciado:"quem é a namorada do mat 4",
+  alternativas:[
+"Amanda",
+"Fernanda",
+ ]
+},
+{
+enunciado:"pergunta 5",
+  alternativas:[
+"alternativa 1",
+"alternativa 2",
+ ]
+},
+];
